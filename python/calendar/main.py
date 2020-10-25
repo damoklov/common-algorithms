a924d22b40ca3020968b7179a7fa226d9fea134f
damoklov <mishanya@protonmail.com>
2020-10-25 21:51:59 +0200

    Added Calendar task

---
+++ python/calendar/main.py
@@ -0,0 +1,20 @@
+from calendar import Calendar, Node
+
+
+if __name__ == '__main__':
+    calendar = Calendar()
+    new_node = Node((5,8))
+    calendar.insert_into_sorted(new_node)
+    new_node = Node((3,5))
+    calendar.insert_into_sorted(new_node)
+    new_node = Node((0, 1))
+    calendar.insert_into_sorted(new_node)
+    new_node = Node((4,8))
+    calendar.insert_into_sorted(new_node)
+    new_node = Node((10,12))
+    calendar.insert_into_sorted(new_node)
+    new_node = Node((9,12))
+    calendar.insert_into_sorted(new_node)
+    print(calendar)
+    calendar.optimize()
+    print(calendar)
