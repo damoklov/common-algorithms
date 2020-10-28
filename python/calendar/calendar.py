301f8f8809295e487e4dd4d46b728c467eb1a9c9
damoklov <mishanya@protonmail.com>
2020-10-29 00:38:50 +0200

    Fixed corner case

--- python/calendar/calendar.py
+++ python/calendar/calendar.py
@@ -23,22 +23,30 @@ class Calendar:
         if self.head is None:
             node_to_insert.next = self.head
             self.head = node_to_insert
-        elif self.head.data[0] >= node_to_insert.data[0]:
+        elif self.head.data[0] > node_to_insert.data[0]:
             node_to_insert.next = self.head
             self.head = node_to_insert
         else:
             current = self.head
             while current.next is not None and current.next.data[0] < node_to_insert.data[0]:
                 current = current.next
-            node_to_insert.next = current.next
-            current.next = node_to_insert
+            if current.next is not None and current.next.data[0] == node_to_insert.data[0]:
+                if current.next.data[1] < node_to_insert.data[1]:
+                    current = current.next
+                    node_to_insert.next = current.next
+                    current.next = node_to_insert
+                else:
+                    node_to_insert.next = current.next
+                    current.next = node_to_insert
+            else:
+                node_to_insert.next = current.next
+                current.next = node_to_insert
 
     def optimize(self):
         current = self.head
         while current.next is not None:
             if current.data[1] >= current.next.data[0]:
-                current.data = (current.data[0], current.next.data[1])
+                current.data = (current.data[0], max(current.next.data[1], current.data[1]))
                 current.next = current.next.next
-                current = current
             else:
                 current = current.next
